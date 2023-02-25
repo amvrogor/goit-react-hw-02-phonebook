0814a196be87870bd3d5953bd904c5a88d9c95e8
Alexey Gorchakov <amvrogor@gmail.com>
2023-02-25 17:38:21 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,61 @@
+import { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactsList } from './ContactsList/ContactsList';
 import { GlobalStyle } from './GlobalStyle';
+import { Layout } from './Layout/Layout';
 
-export const App = () => {
-  return (
-    <div>
-      <GlobalStyle />
-    </div>
-  );
-};
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  addContact = newContact => {
+    this.setState(prevState => {
+      return prevState.contacts
+        .map(contact => contact.name.toLowerCase())
+        .includes(newContact.name.toLowerCase())
+        ? alert(`${newContact.name} is already in contacts`)
+        : { contacts: [...prevState.contacts, newContact] };
+    });
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getFilteredContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(
+          contact => contact.id !== contactId
+        ),
+      };
+    });
+  };
+
+  render() {
+    const FilteredContacts = this.getFilteredContacts();
+    return (
+      <Layout title="Phonebook">
+        <ContactForm onSave={this.addContact} />
+        <Filter value={this.state.filter} onChange={this.changeFilter} />
+        <ContactsList
+          contacts={FilteredContacts}
+          onDelete={this.deleteContact}
+        />
+        <GlobalStyle />
+      </Layout>
+    );
+  }
+}
